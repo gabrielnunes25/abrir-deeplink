58cefbc38296dbd132a3ac287c30c1aa3cf773dc
Gabriel Nunes Silva <gabriel.silva@globalsys.com.br>
2022-03-16 09:58:08 -0300

    Deletando arquivo desnecessario

--- script.js
+++
@@ -1,7 +0,0 @@
-function updateLink() {
-  const input = document.getElementById("deeplink-input");
-
-  const button = document.getElementById("deeplink-button");
-
-  button.setAttribute("href", input.value);
-}
